7ff760520aabb2512284c5246cf5c3c2df590296
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-11-23 00:52:00 +0200

    updated OptionSelectorLab4

--- src/lab4/view/OptionSelectorLab4.java
+++ src/lab4/view/OptionSelectorLab4.java
@@ -1,5 +1,7 @@
 package lab4.view;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class OptionSelectorLab4 {
@@ -12,11 +14,12 @@ public class OptionSelectorLab4 {
     enum Mode {
         CONSOLE, FILE, LENGTH_ANALIZ
     }
-    enum ErrorsInput{
+
+    enum ErrorsInput {
         BY_INDEX, BY_NUMBER
     }
 
-    public Mode selectMode(){
+    public Mode selectMode() {
         final String selectMode = """
                 Please, select mode of program
                 Type 0 if you want to input your message from console
@@ -24,43 +27,71 @@ public class OptionSelectorLab4 {
                 Type any other symbol if you want to analyze files with different length and errors number
                 """;
         System.out.print(selectMode + "\nEnter: ");
-        try(Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
             String mode = sc.nextLine();
-            if(mode.contains("0")){
+            if (mode.contains("0")) {
                 modeOption = Mode.CONSOLE;
                 System.out.println("Enter your message: ");
             } else if (mode.contains("1")) {
                 modeOption = Mode.FILE;
                 System.out.println("Enter your path to your file: ");
-            }else{
+            } else {
                 modeOption = Mode.LENGTH_ANALIZ;
             }
-            if(modeOption != Mode.LENGTH_ANALIZ){
+            if (modeOption != Mode.LENGTH_ANALIZ) {
                 message = sc.nextLine();
             }
         }
         return modeOption;
     }
 
-    public ErrorsInput selectWayOfErrorInput(){
+    public ErrorsInput selectWayOfErrorInput() {
         final String selectWay = """
                 Enter 0 if you want to set error by index
                 Enter any other symbol if you want to select number of random errors
                 """;
         System.out.print(selectWay + "\nEnter: ");
-        try(Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
             final String choice = sc.nextLine();
-            if(choice.contains("0")){
-                System.out.print("Index: ");
-            }else{
+            if (choice.contains("0")) {
+                errorsInputOption = ErrorsInput.BY_INDEX;
+                System.out.print("How many errors do you want to enter?\nEnter: ");
+            } else {
+                errorsInputOption = ErrorsInput.BY_NUMBER;
                 System.out.print("Number: ");
             }
-            this.number = Integer.parseInt(sc.nextLine());
-        }catch (NumberFormatException e){
-            System.out.println("You have entered bad number and it was replaced with 1 by default");
-            this.number = 1;
+            this.number = parseInt(sc.nextLine(), 1);
         }
         return errorsInputOption;
+    }
+
+    public int inputNumberAttempts(){
+        System.out.println("How many attempts of fixing should Hamming do?");
+        System.out.print("Enter");
+        try (Scanner sc = new Scanner(System.in)) {
+            return parseInt(sc.nextLine(), 2);
+        }
+    }
+
+    private static int parseInt(final String line, final int defaultValue) {
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException e) {
+            System.out.println("You have entered bad number and it was replaced with " + defaultValue + " by default");
+            return defaultValue;
+        }
+    }
+
+    public List<Integer> inputList(int number) {
+        List<Integer> list = new LinkedList<>();
+        try (Scanner sc = new Scanner(System.in)) {
+            for (int i = 1; i <= number; i++) {
+                System.out.print("Number" + i + ": ");
+                var value = parseInt(sc.nextLine(), 1);
+                list.add(value);
+            }
+        }
+        return list;
     }
 
     public String getMessage() {
